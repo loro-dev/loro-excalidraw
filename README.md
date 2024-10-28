4c48a2e46474a69744a1ca7722e7b84cbb73303c
Zixuan Chen <remch183@outlook.com>
2024-10-28 10:44:49 +0800

    fix: show correct size info for new version

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Loro Excalidraw Example
 
-- This demo serves as a proof of concept for the Loro Excalidraw integration. It's not optimized for production use.
+- This demo serves as a proof of concept for the Loro Excalidraw integration.
+  It's not optimized for production use.
 - Play online at https://loro-excalidraw.vercel.app/
-- This demo simulates syncing via BroadcastChannel. You can open several tabs to simulate multiplayer. 
-  - Currently, if you open a new tab after editing, the new tab won't get the old updates from the others
+- This demo simulates syncing via BroadcastChannel. You can open several tabs to
+  simulate multiplayer.
 
 https://github.com/loro-dev/loro-excalidraw/assets/18425020/2fd9c2cc-9c9d-4e9d-9328-69ceede4a75e
-
--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import { useEffect, useMemo, useRef, useState } from 'react'
-import '@radix-ui/themes/styles.css';
-import { Excalidraw } from '@excalidraw/excalidraw';
-import { Slider } from '@radix-ui/themes';
-import { LoroDoc, LoroList, LoroMap, OpId, VersionVector } from 'loro-crdt';
-import deepEqual from 'deep-equal';
-import './App.css'
-import { ExcalidrawImperativeAPI } from '@excalidraw/excalidraw/types/types';
+import { useEffect, useMemo, useRef, useState } from "react";
+import "@radix-ui/themes/styles.css";
+import { Excalidraw } from "@excalidraw/excalidraw";
+import { Slider } from "@radix-ui/themes";
+import { LoroDoc, LoroList, LoroMap, OpId, VersionVector } from "loro-crdt";
+import deepEqual from "deep-equal";
+import "./App.css";
+import { ExcalidrawImperativeAPI } from "@excalidraw/excalidraw/types/types";
 import pako from "pako";
 
-
 function opIdToString(id: OpId): string {
-  return `${id.counter}@${id.peer.toString()}`
+  return `${id.counter}@${id.peer.toString()}`;
 }
 
 function frontierToString(frontier: OpId[]): string {
-  return frontier.map(opIdToString).join(",")
+  return frontier.map(opIdToString).join(",");
 }
 
 function frontiersToString(frontiers: OpId[][]): string {
@@ -25,8 +24,8 @@ function stringToOpId(str: string): OpId {
   const [counter, peer] = str.split("@");
   return {
     counter: parseInt(counter),
-    peer: peer as `${number}`
-  }
+    peer: peer as `${number}`,
+  };
 }
 
 function stringToFrontier(str: string): OpId[] {
@@ -45,26 +44,24 @@ function App() {
   const excalidrawAPI = useRef<ExcalidrawImperativeAPI>();
   const versionsRef = useRef<OpId[][]>([]);
   const [docSize, setDocSize] = useState({
-    oldUpdates: 0,
-    newUpdates: 0,
-    oldSnapshot: 0,
-    newSnapshot: 0
+    updates: 0,
+    shallowSnapshot: 0,
+    snapshot: 0,
   });
   const [compressedDocSize, setCompressedDocSize] = useState({
-    oldUpdates: 0,
-    newUpdates: 0,
-    oldSnapshot: 0,
-    newSnapshot: 0
+    updates: 0,
+    shallowSnapshot: 0,
+    snapshot: 0,
   });
   const [maxVersion, setMaxVersion] = useState(-1);
-  const [vv, setVV] = useState("")
+  const [vv, setVV] = useState("");
   const channel = useMemo(() => {
     return new BroadcastChannel("temp");
   }, []);
   useEffect(() => {
     return () => {
       channel.close();
-    }
+    };
   }, [channel]);
   const [versionNum, setVersionNum] = useState(-1);
 
@@ -75,13 +72,12 @@ function App() {
       const versions = localStorage.getItem("frontiers");
       versionsRef.current = stringToFrontiers(versions || "");
       setMaxVersion(versionsRef.current.length - 1);
-      setVersionNum(versionsRef.current.length - 1)
+      setVersionNum(versionsRef.current.length - 1);
     });
 
     const docElements = doc.getList("elements");
     let lastVersion: VersionVector | undefined = undefined;
-    channel.onmessage = e => {
-      console.log("Event");
+    channel.onmessage = (e) => {
       const bytes = new Uint8Array(e.data);
       try {
         doc.import(bytes);
@@ -92,71 +88,77 @@ function App() {
     };
     doc.subscribe((e) => {
       const version = Object.fromEntries(doc.version().toJSON());
-      let vv = ""
+      let vv = "";
       for (const [k, v] of Object.entries(version)) {
-        vv += `${k.toString().slice(0, 4)}:${v} `
+        vv += `${k.toString().slice(0, 4)}:${v} `;
       }
 
       setVV(vv);
       if (e.by === "local") {
-        const bytes = doc.export({mode: 'update', from: lastVersion});
+        const bytes = doc.export({ mode: "update", from: lastVersion });
         lastVersion = doc.version();
         channel.postMessage(bytes);
       }
       if (e.by !== "checkout") {
-        versionsRef.current.push(doc.frontiers())
+        versionsRef.current.push(doc.frontiers());
         setMaxVersion(versionsRef.current.length - 1);
-        setVersionNum(versionsRef.current.length - 1)
-        const data = doc.export({mode: 'update'});
-        localStorage.setItem("store", btoa(String.fromCharCode(...data)));
-        localStorage.setItem("frontiers", frontiersToString(versionsRef.current));
-        const newSnapshot = doc.export({
-          mode: 'shallow-snapshot',
-          frontiers: doc.frontiers(),
+        setVersionNum(versionsRef.current.length - 1);
+        const updates = doc.export({ mode: "update" });
+        let str = "";
+        for (let i = 0; i < updates.length; i++) {
+          str += String.fromCharCode(updates[i]);
+        }
+        localStorage.setItem("store", btoa(str));
+        localStorage.setItem(
+          "frontiers",
+          frontiersToString(versionsRef.current),
+        );
+        const snapshot = doc.export({
+          mode: "snapshot",
         });
-        const oldSnapshot = doc.export({
-          mode: 'shallow-snapshot',
-          frontiers: doc.frontiers(),
-        });
-        const oldUpdates = doc.export({
-          mode: 'shallow-snapshot',
+        const shallowSnapshot = doc.export({
+          mode: "shallow-snapshot",
           frontiers: doc.frontiers(),
         });
         setDocSize({
-          newUpdates: data.length,
-          newSnapshot: newSnapshot.length,
-          oldSnapshot: oldSnapshot.length,
-          oldUpdates: oldUpdates.length
+          updates: updates.length,
+          snapshot: snapshot.length,
+          shallowSnapshot: shallowSnapshot.length,
         });
         setCompressedDocSize({
-          newUpdates: getCompressedSize(data),
-          newSnapshot: getCompressedSize(newSnapshot),
-          oldSnapshot: getCompressedSize(oldSnapshot),
-          oldUpdates: getCompressedSize(oldUpdates)
-        })
+          updates: getCompressedSize(updates),
+          snapshot: getCompressedSize(snapshot),
+          shallowSnapshot: getCompressedSize(shallowSnapshot),
+        });
       }
       if (e.by !== "local") {
-        excalidrawAPI.current?.updateScene({ elements: docElements.toJSON() })
+        excalidrawAPI.current?.updateScene({ elements: docElements.toJSON() });
       }
     });
     setTimeout(() => {
       if (data && data?.length > 0) {
-        const bytes = new Uint8Array(atob(data).split("").map(function (c) { return c.charCodeAt(0) }));
+        const bytes = new Uint8Array(
+          atob(data).split("").map(function (c) {
+            return c.charCodeAt(0);
+          }),
+        );
         doc.checkoutToLatest();
         doc.import(bytes);
         setMaxVersion(versionsRef.current.length - 1);
-        setVersionNum(versionsRef.current.length - 1)
+        setVersionNum(versionsRef.current.length - 1);
       }
     }, 100);
-    return { doc, docElements }
+    return { doc, docElements };
   }, [channel]);
 
   const lastVersion = useRef(-1);
   return (
-    <div >
+    <div>
       <div style={{ width: "100%", height: "calc(100vh - 100px)" }}>
         <Excalidraw
-          excalidrawAPI={api => { excalidrawAPI.current = api }}
+          excalidrawAPI={(api) => {
+            excalidrawAPI.current = api;
+          }}
           viewModeEnabled={versionNum !== maxVersion}
           onChange={(elements) => {
             const v = getVersion(elements);
@@ -175,62 +177,102 @@ function App() {
         />
       </div>
       <div style={{ margin: "1em 2em", zIndex: 7 }}>
-        <div style={{ fontSize: "0.8em", width: "100%", display: "flex", flexDirection: "row", position: "relative", justifyContent: "flex-start" }}>
+        <div
+          style={{
+            fontSize: "0.8em",
+            width: "100%",
+            display: "flex",
+            flexDirection: "row",
+            position: "relative",
+            justifyContent: "flex-start",
+          }}
+        >
           <div>
-            <button onClick={() => {
-              localStorage.clear();
-              location.reload();
-            }}>Clear</button> Version Vector {vv}
+            <button
+              onClick={() => {
+                localStorage.clear();
+                location.reload();
+              }}
+            >
+              Clear
+            </button>{" "}
+            Version Vector {vv}
           </div>
-          <div style={{ position: "absolute", right: 16, bottom: 0, zIndex: 7 }}>
+          <div
+            style={{
+              position: "absolute",
+              right: 16,
+              bottom: 0,
+              zIndex: 7,
+              textAlign: "right",
+            }}
+          >
             <p>
-              New updates size {docSize.newUpdates} bytes {compressedDocSize.newUpdates}
+              Updates size {docSize.updates} bytes; Compressed size{" "}
+              {compressedDocSize.updates} bytes
             </p>
             <p>
-              Old updates size {docSize.oldUpdates} bytes {compressedDocSize.oldUpdates}
+              Snapshot size {docSize.snapshot} bytes; Compressed size{" "}
+              {compressedDocSize.snapshot} bytes
             </p>
             <p>
-              New snapshot size {docSize.newSnapshot} bytes {compressedDocSize.newSnapshot}
-            </p>
-            <p>
-              Old snapshot size {docSize.oldSnapshot} bytes {compressedDocSize.oldSnapshot}
+              Shallow snapshot size {docSize.shallowSnapshot}{" "}
+              bytes; Compressed size {compressedDocSize.shallowSnapshot} bytes
             </p>
           </div>
         </div>
-        <Slider value={[versionNum]} min={-1} max={maxVersion} onValueChange={(v) => {
-          setVersionNum(v[0]);
-          if (v[0] === -1) {
-            doc.checkout([]);
-          } else {
-            if (v[0] === versionsRef.current.length - 1) {
-              doc.checkoutToLatest()
+        <Slider
+          value={[versionNum]}
+          min={-1}
+          max={maxVersion}
+          onValueChange={(v) => {
+            setVersionNum(v[0]);
+            if (v[0] === -1) {
+              doc.checkout([]);
             } else {
-              doc.checkout(versionsRef.current[v[0]]);
+              if (v[0] === versionsRef.current.length - 1) {
+                doc.checkoutToLatest();
+              } else {
+                doc.checkout(versionsRef.current[v[0]]);
+              }
             }
-          }
-        }} />
+          }}
+        />
       </div>
     </div>
-  )
+  );
 }
 
 function getCompressedSize(data: Uint8Array): number {
   try {
-    return pako.deflateRaw(data).length
+    return pako.deflateRaw(data).length;
   } catch (e) {
-    return 0
+    return 0;
   }
 }
 
-function recordLocalOps(loroList: LoroList, elements: readonly { version: number }[]): boolean {
+function recordLocalOps(
+  loroList: LoroList,
+  elements: readonly { version: number; isDeleted?: boolean }[],
+): boolean {
+  elements = elements.filter((e) => !e.isDeleted);
   let changed = false;
   for (let i = loroList.length; i < elements.length; i++) {
     loroList.insertContainer(i, new LoroMap());
     changed = true;
   }
+  if (elements.length < loroList.length) {
+    loroList.delete(elements.length, loroList.length - elements.length);
+    changed = true;
+  }
 
-  for (let i = 0; i < elements.length; i++) {
-    const map = loroList.get(i) as LoroMap;
+  const n = elements.length;
+  for (let i = 0; i < n; i++) {
+    const map = loroList.get(i) as LoroMap | undefined;
+    if (!map) {
+      break;
+    }
+
     const elem = elements[i];
     if (map.get("version") === elem.version) {
       continue;
@@ -238,20 +280,23 @@ function recordLocalOps(loroList: LoroList, elements: readonly { version: number
 
     for (const [key, value] of Object.entries(elem)) {
       const src = map.get(key);
-      if ((typeof src === "object" && !deepEqual(map.get(key), value)) || src !== value) {
+      if (
+        (typeof src === "object" && !deepEqual(map.get(key), value)) ||
+        src !== value
+      ) {
         changed = true;
-        map.set(key, value)
+        map.set(key, value);
       }
     }
   }
 
-  return changed
+  return changed;
 }
 
 function getVersion(elems: readonly { version: number }[]): number {
   return elems.reduce((acc, curr) => {
-    return curr.version + acc
-  }, 0)
+    return curr.version + acc;
+  }, 0);
 }
 
-export default App
+export default App;
